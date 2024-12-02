be75c668a788d37531c9035b9e73bbd15c1765b4
Calestial Ashley <calestialashley@gmail.com>
2024-12-02 11:39:58 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  animations: {
+    galaxySpin: './javaScript-animations/Galaxy Spiral/galaxy-spin.js',
+    galaxySpiralCSS: './javaScript-animations/Galaxy Spiral/galaxy-spiral.css',
+    asteroid: './javaScript-animations/asteroid/asteroid-belt.css',
+    asteroidJS: './javaScript-animations/asteroid/asteroid.js',
+    starAnimation: './javaScript-animations/star-animation/star-animation.css',
+    starScript: './javaScript-animations/star-animation/star-script.js'
+  },
+  styles: {
+    cosmicExplosion: './cosmic-explosion.css',
+    planetOrbit: './planet-orbit.css',
+    pulsar: './pulsar.css',
+    cyberNeon: './cyber-neon.css',
+    glowAnimation: './glow-animation.css',
+    minimalLight: './minimal-light.css',
+    modernChat: './modern-chat.css',
+    modernTerminal: './modern-terminal.css',
+    neonMinimalism: './neon-minimalism.css',
+    readability: './readability.css',
+    sleekDark: './sleek-dark.css',
+    vibrantGradient: './vibrant-gradient.css'
+  }
+};
